b5a1489230da339156e0785389e8c944185ba9ca
guptabhai <144488594+guptabhai@users.noreply.github.com>
2023-09-18 15:37:12 +0530

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b;
+cout<<"enter the value of a :";
+cin>>a;
+cout<<"enter the value of b :";
+cin>>b;
+
+cout<<"the value of a+b is :"<<a+b<<endl;
+    return 0;
+}
